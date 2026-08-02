bcb1a1fe74366667f2c1b1b8b9c881dc2befdec1
Shashank2810 <49634842+Shashank2810@users.noreply.github.com>
2026-08-02 16:09:37 +0530

    Delete ytdr directory

--- ytdr/downloader/__init__.py
+++
@@ -1,7 +0,0 @@
-from .models import VideoFormat
-from .models import VideoInfo
-
-__all__ = [
-    "VideoFormat",
-    "VideoInfo",
-]
--- ytdr/models/__init__.py
+++
@@ -1,7 +0,0 @@
-from .download import Download
-from .download import DownloadStatus
-
-__all__ = [
-    "Download",
-    "DownloadStatus",
-]
